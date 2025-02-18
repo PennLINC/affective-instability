ebaacd60fb530f7e104a1247431f9f2b6c9ab53e
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-02-18 16:29:14 -0500

    Update 04b_fix_asl.py

--- curation/04b_fix_asl.py
+++ curation/04b_fix_asl.py
@@ -56,79 +56,73 @@ if __name__ == "__main__":
             scans_df = pd.read_table(scans_file)
 
             perf_dir = os.path.join(session_dir, "perf")
-
-            # Split m0scan into two volumes under the anat directory.
             anat_dir = os.path.join(session_dir, "anat")
-            anat_files = sorted(glob(os.path.join(anat_dir, "*_TDP.nii.gz")))
-            if len(anat_files) == 0:
-                # No TDP scans, so we need to split the m0scan into two volumes.
-                # The first and second are TDP scans, but the first one is also used as the m0scan.
-                m0scan_files = sorted(glob(os.path.join(perf_dir, "*_m0scan.nii.gz")))
-                for m0scan_file in m0scan_files:
-                    img = nb.load(m0scan_file)
-                    n_vols = 1
-                    if img.ndim == 4:
-                        n_vols = img.shape[3]
 
-                    if n_vols == 1:
-                        print(f"M0scan has only one volume: {m0scan_file}")
-                    else:
-                        tdp1_file = os.path.join(anat_dir, f"{sub_id}_{ses_id}_acq-tr1_TDP.nii.gz")
-                        first_img = img.slicer[..., 0]
-                        first_img.to_filename(tdp1_file)
-                        tdp2_file = os.path.join(anat_dir, f"{sub_id}_{ses_id}_acq-tr2_TDP.nii.gz")
-                        second_img = img.slicer[..., 1]
-                        second_img.to_filename(tdp2_file)
+            # The first and second are TDP scans, but the first one is also used as the m0scan.
+            m0scan_files = sorted(glob(os.path.join(perf_dir, "*_m0scan.nii.gz")))
+            for m0scan_file in m0scan_files:
+                print(f"Processing {os.path.basename(m0scan_file)}")
+                img = nb.load(m0scan_file)
+                n_vols = 1
+                if img.ndim == 4:
+                    n_vols = img.shape[3]
 
-                        # Add new files to the scans.tsv file.
-                        m0scan_fname = os.path.join("perf", os.path.basename(m0scan_file))
+                if n_vols == 1:
+                    print(f"M0scan has only one volume: {m0scan_file}")
+                else:
+                    tdp1_file = os.path.join(anat_dir, f"{sub_id}_{ses_id}_acq-tr1_TDP.nii.gz")
+                    first_img = img.slicer[..., 0]
+                    first_img.to_filename(tdp1_file)
+                    tdp2_file = os.path.join(anat_dir, f"{sub_id}_{ses_id}_acq-tr2_TDP.nii.gz")
+                    second_img = img.slicer[..., 1]
+                    second_img.to_filename(tdp2_file)
 
-                        i_row = len(scans_df.index)
-                        tdp1_fname = os.path.join("anat", os.path.basename(tdp1_file))
-                        scans_df.loc[i_row] = scans_df.loc[
-                            scans_df["filename"] == m0scan_fname
-                        ].iloc[0]
-                        scans_df.loc[i_row, "filename"] = tdp1_fname
+                    # Add new files to the scans.tsv file.
+                    m0scan_fname = os.path.join("perf", os.path.basename(m0scan_file))
 
-                        i_row = len(scans_df.index)
-                        tdp2_fname = os.path.join("anat", os.path.basename(tdp2_file))
-                        scans_df.loc[i_row] = scans_df.loc[
-                            scans_df["filename"] == m0scan_fname
-                        ].iloc[0]
-                        scans_df.loc[i_row, "filename"] = tdp2_fname
+                    i_row = len(scans_df.index)
+                    tdp1_fname = os.path.join("anat", os.path.basename(tdp1_file))
+                    scans_df.loc[i_row] = scans_df.loc[scans_df["filename"] == m0scan_fname].iloc[0]
+                    scans_df.loc[i_row, "filename"] = tdp1_fname
 
-                        # Overwrite the two-volume m0scan file with the first volume.
-                        first_img.to_filename(m0scan_file)
+                    i_row = len(scans_df.index)
+                    tdp2_fname = os.path.join("anat", os.path.basename(tdp2_file))
+                    scans_df.loc[i_row] = scans_df.loc[scans_df["filename"] == m0scan_fname].iloc[0]
+                    scans_df.loc[i_row, "filename"] = tdp2_fname
 
-                    # Add RepetitionTimePreparation to the m0scan JSON
-                    m0scan_json = m0scan_file.replace(".nii.gz", ".json")
-                    with open(m0scan_json, "r") as f:
-                        m0scan_metadata = json.load(f)
+                    # Overwrite the two-volume m0scan file with the first volume.
+                    first_img.to_filename(m0scan_file)
 
-                    m0scan_metadata["RepetitionTimePreparation"] = m0scan_metadata["RepetitionTime"]
+                # Add RepetitionTimePreparation to the m0scan JSON
+                m0scan_json = m0scan_file.replace(".nii.gz", ".json")
+                with open(m0scan_json, "r") as f:
+                    m0scan_metadata = json.load(f)
 
-                    # Copy the m0scan JSON file to the TDP scans.
-                    tdp1_json = os.path.join(anat_dir, f"{sub_id}_{ses_id}_acq-tr1_TDP.json")
-                    with open(tdp1_json, "w") as f:
-                        json.dump(m0scan_metadata, f, indent=4, sort_keys=True)
+                m0scan_metadata["RepetitionTimePreparation"] = m0scan_metadata["RepetitionTime"]
 
-                    tdp2_json = os.path.join(anat_dir, f"{sub_id}_{ses_id}_acq-tr2_TDP.json")
-                    with open(tdp2_json, "w") as f:
-                        json.dump(m0scan_metadata, f, indent=4, sort_keys=True)
+                # Copy the m0scan JSON file to the TDP scans.
+                tdp1_json = os.path.join(anat_dir, f"{sub_id}_{ses_id}_acq-tr1_TDP.json")
+                with open(tdp1_json, "w") as f:
+                    json.dump(m0scan_metadata, f, indent=4, sort_keys=True)
 
-                    # Add IntendedFor to the m0scan metadata
-                    # XXX: ASLPrep doesn't support BIDS-URIs yet.
-                    m0scan_metadata["IntendedFor"] = [
-                        m0scan_file.replace("_m0scan.", "_asl.").replace(
-                            os.path.join(dset_dir, sub_id) + "/", ""
-                        )
-                    ]
-                    with open(m0scan_json, "w") as f:
-                        json.dump(m0scan_metadata, f, indent=4, sort_keys=True)
+                tdp2_json = os.path.join(anat_dir, f"{sub_id}_{ses_id}_acq-tr2_TDP.json")
+                with open(tdp2_json, "w") as f:
+                    json.dump(m0scan_metadata, f, indent=4, sort_keys=True)
+
+                # Add IntendedFor to the m0scan metadata
+                # XXX: ASLPrep doesn't support BIDS-URIs yet.
+                m0scan_metadata["IntendedFor"] = [
+                    m0scan_file.replace("_m0scan.", "_asl.").replace(
+                        os.path.join(dset_dir, sub_id) + "/", ""
+                    )
+                ]
+                with open(m0scan_json, "w") as f:
+                    json.dump(m0scan_metadata, f, indent=4, sort_keys=True)
 
             # Patch hardcoded metadata into the asl.json files.
             asl_jsons = sorted(glob(os.path.join(perf_dir, "*_asl.json")))
             for asl_json in asl_jsons:
+                print(f"Processing {os.path.basename(asl_json)}")
                 with open(asl_json, "r") as f:
                     asl_metadata = json.load(f)
 
