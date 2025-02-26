f22a5c0af25a4ee9d69b9ecfbe9c7351b9b9f942
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-02-26 12:34:38 -0500

    Fix up events file script.

--- curation/05_copy_events.py
+++ curation/05_copy_events.py
@@ -1,10 +1,15 @@
+"""Copy events files downloaded from Flywheel into the BIDS dataset."""
+
 import os
+import shutil
 from glob import glob
 
 
 if __name__ == "__main__":
     out_dir = "/cbica/projects/pafin/dset"
-    in_files = sorted(glob("/cbica/projects/pafin/sourcedata/imaging/scitran/bbl/PAFIN_844353/*_*/*_events.tsv"))
+    in_files = sorted(
+        glob("/cbica/projects/pafin/sourcedata/imaging/scitran/bbl/PAFIN_844353/*_*/*_events.tsv")
+    )
 
     task_dict = {
         "task-Bao": "task-bao",
@@ -16,6 +21,8 @@ if __name__ == "__main__":
     }
     for in_file in in_files:
         fname = os.path.basename(in_file)
+
+        # Create appropriate output filename
         subject_id = fname.split("_")[0]
         session_id = "ses-1"
         task_id = fname.split("_")[1]
@@ -23,3 +30,7 @@ if __name__ == "__main__":
         dir_id = dir_dict[task_id]
         file_out_dir = os.path.join(out_dir, subject_id, session_id, "func")
         out_fname = f"{subject_id}_{session_id}_{task_id}_{dir_id}_run-01_events.tsv"
+
+        # Copy the file
+        out_file = os.path.join(file_out_dir, out_fname)
+        shutil.copy(in_file, out_file)
