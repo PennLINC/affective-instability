8e1e0d55a8d9e8cc21c9f6298361e8588520bd78
tsalo <salot@pennmedicine.upenn.edu>
2025-03-10 16:13:34 -0400

    Fix up the plotting code.

--- analysis/plot_qsirecon_scalar_maps.py
+++ analysis/plot_qsirecon_scalar_maps.py
@@ -20,24 +20,27 @@ if __name__ == "__main__":
     for title, pattern in patterns.items():
         # Get all scalar maps
         scalar_maps = sorted(glob(os.path.join(in_dir, pattern)))
+        scalar_maps = [f for f in scalar_maps if "PILOT" not in f]
         print(f"{title}: {len(scalar_maps)}")
 
-        mean_img = image.mean_img(scalar_maps)
+        mean_img = image.mean_img(scalar_maps, copy_header=True)
         sd_img = image.math_img("np.std(img, axis=3)", img=scalar_maps)
 
         # Plot mean and SD
-        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+        fig, axs = plt.subplots(2, 1, figsize=(10, 5))
         plotting.plot_stat_map(
             mean_img,
             display_mode="z",
-            cut_coords=[-30, -20, -10, 0, 10, 20, 30, 40, 50, 60],
+            cut_coords=[-30, -15, 0, 15, 30, 45, 60],
+            title="Mean",
             axes=axs[0],
             figure=fig,
         )
         plotting.plot_stat_map(
             sd_img,
             display_mode="z",
-            cut_coords=[-30, -20, -10, 0, 10, 20, 30, 40, 50, 60],
+            cut_coords=[-30, -15, 0, 15, 30, 45, 60],
+            title="Standard Deviation",
             axes=axs[1],
             figure=fig,
         )
