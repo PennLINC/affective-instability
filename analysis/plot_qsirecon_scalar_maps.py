0f35e38cc299d4156562218b0923e2c94e690060
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-03-10 15:54:16 -0400

    Draft analysis code.

---
+++ analysis/plot_qsirecon_scalar_maps.py
@@ -0,0 +1,44 @@
+"""Plot scalar maps from QSIRecon."""
+
+import os
+from glob import glob
+
+import matplotlib.pyplot as plt
+from nilearn import image, plotting
+
+
+if __name__ == "__main__":
+    in_dir = "/cbica/projects/pafin/derivatives/qsirecon"
+    out_dir = "../figures"
+
+    patterns = {
+        "NODDI ICVF": "qsirecon-wmNODDI/sub-*/ses-1/dwi/*_space-MNI152NLin2009cAsym_model-noddi_param-icvf_dwimap.nii.gz",
+        "TORTOISE RTOP": "qsirecon-TORTOISE_model-MAPMRI/sub-*/ses-1/dwi/*_space-MNI152NLin2009cAsym_model-mapmri_param-rtop_dwimap.nii.gz",
+        "DSIStudio GQI FA": "qsirecon-DSIStudioGQI/sub-*/ses-1/dwi/*_space-MNI152NLin2009cAsym_model-tensor_param-fa_dwimap.nii.gz",
+        "DSIStudio GQI MD": "qsirecon-DSIStudioGQI/sub-*/ses-1/dwi/*_space-MNI152NLin2009cAsym_model-tensor_param-md_dwimap.nii.gz",
+    }
+    for title, pattern in patterns.items():
+        # Get all scalar maps
+        scalar_maps = sorted(glob(pattern))
+        print(f"{title}: {len(scalar_maps)}")
+
+        mean_img = image.mean_img(scalar_maps)
+        sd_img = image.math_img("np.std(img, axis=3)", img=scalar_maps)
+
+        # Plot mean and SD
+        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+        plotting.plot_stat_map(
+            mean_img,
+            display_mode="z",
+            cut_coords=[-30, -20, -10, 0, 10, 20, 30, 40, 50, 60],
+            ax=axs[0],
+        )
+        plotting.plot_stat_map(
+            sd_img,
+            display_mode="z",
+            cut_coords=[-30, -20, -10, 0, 10, 20, 30, 40, 50, 60],
+            ax=axs[1],
+        )
+        fig.suptitle(title)
+        plt.savefig(os.path.join(out_dir, f"{title.replace(' ', '_')}.png"))
+        plt.close()
