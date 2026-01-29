38646580dda0a064eea4298d4677f8681133ada0
tsalo <salot@pennmedicine.upenn.edu>
2026-01-29 15:33:06 -0500

    Update QSIRecon scripts.

--- processing/pull_singularity_images.sh
+++ processing/pull_singularity_images.sh
@@ -3,5 +3,6 @@
 #singularity pull --name /cbica/projects/pafin/apptainer/aslprep-0.7.5.sif docker://pennlinc/aslprep:0.7.5
 #singularity pull --name /cbica/projects/pafin/apptainer/qsiprep-1.0.0.sif docker://pennlinc/qsiprep:1.0.0
 #singularity pull --name /cbica/projects/pafin/apptainer/qsirecon-1.0.1.sif docker://pennlinc/qsirecon:1.0.1
+singularity pull --name /cbica/projects/pafin/apptainer/qsirecon-1.1.1.sif docker://pennlinc/qsirecon:1.1.1
 #singularity pull --name /cbica/projects/pafin/apptainer/fmripost_aroma-0.0.10.sif docker://nipreps/fmripost-aroma:0.0.10
-singularity pull --name /cbica/projects/pafin/apptainer/xcp_d-0.10.6.sif docker://pennlinc/xcp_d:0.10.6
+#singularity pull --name /cbica/projects/pafin/apptainer/xcp_d-0.10.6.sif docker://pennlinc/xcp_d:0.10.6
